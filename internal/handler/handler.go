1e036dfe7d4de5b315c550a80b284e12cc177cc2
Phong vo <phongvxk38@gmail.com>
2021-03-01 22:32:04 +0700

    Update log

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 
 func testGracefulShutDown(res http.ResponseWriter, req *http.Request) {
 	time.Sleep(10 * time.Second)
+	log.Println("testGracefulShutdown job completed")
 	response.ResponseWithJSON(res, 200, map[string]interface{}{"status": "completed"})
 }
 
